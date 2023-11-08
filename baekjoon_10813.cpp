4e7621bedecd1716a8461fec705418ff358ceb58
박건우 <136959177+chaos1231107@users.noreply.github.com>
2023-11-08 13:57:07 +0900

    change ball in ith and jth ball

---
+++ baekjoon_10813.cpp
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int N, M;
+    //int i, j;
+    //int temp;
+    scanf("%d %d", &N, &M);
+    int *ary = (int *)malloc(N * sizeof(int));
+
+    for (int x = 0; x < N; x++)
+    {
+        ary[x] = x+1;
+    }
+
+    for (int k = 0; k < M; k++)
+    {
+        //int temp;
+        int i, j;
+        scanf("%d %d", &i, &j);
+        i--;
+        j--;
+
+        int temp = ary[i];
+        ary[i] = ary[j];
+        ary[j] = temp;
+    }
+
+    for (int y = 0; y < N; y++)
+    {
+        printf("%d ", ary[y]);
+    }
+
+
+    free(ary);
+
+    return 0;
+}
